0ffcbfbf1feb60fa9e2b6f07a2c8dc3599064ada
haroon10725 <104259212+haroon10725@users.noreply.github.com>
2026-03-02 11:48:51 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { insertCoin } from 'playroomkit'
+
+const root = createRoot(document.getElementById('root'))
+
+async function start() {
+  await insertCoin()
+
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+}
+
+start()
